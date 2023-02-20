112c1ded8c14ce53f0c95a821ba875cc227f72d0
burrasrikanth <burrasrikanth@createiq>
2023-02-20 10:33:13 +0530

    join table application

---
+++ HiberbnateJoinTableInheritanceApp/src/main/java/com/ciq/model/Player.java
@@ -0,0 +1,64 @@
+package com.ciq.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+
+@Entity
+@Table
+@Inheritance(strategy = InheritanceType.JOINED)
+public class Player {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column
+	private String name;
+	@Column
+	private String bodyType;
+	
+	public Player() {
+	 
+	}
+
+	public Player( String name, String bodyType) {
+		this.name = name;
+		this.bodyType = bodyType;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBodyType() {
+		return bodyType;
+	}
+
+	public void setBodyType(String bodyType) {
+		this.bodyType = bodyType;
+	}
+
+	@Override
+	public String toString() {
+		return "Player [id=" + id + ", name=" + name + ", bodyType=" + bodyType + "]";
+	}
+	
+	
+	
+	
+
+}
