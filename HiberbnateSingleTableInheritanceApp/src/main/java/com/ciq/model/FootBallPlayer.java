383aab3274558f5ab739961a3461b0e07534fbd4
burrasrikanth <burrasrikanth@createiq>
2023-02-20 10:27:49 +0530

    table per class with parent class

---
+++ HiberbnateSingleTableInheritanceApp/src/main/java/com/ciq/model/FootBallPlayer.java
@@ -0,0 +1,53 @@
+package com.ciq.model;
+
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table
+@DiscriminatorValue(value = "ck_type")
+public class FootBallPlayer extends Player {
+
+	@Column
+	private double noOfGoals;
+	@Column
+	private String teamName;
+
+	public FootBallPlayer() {
+	 
+	}
+
+	 
+
+	public FootBallPlayer(String name, String bodyType, double noOfGoals, String teamName) {
+		super(name, bodyType);
+		this.noOfGoals = noOfGoals;
+		this.teamName = teamName;
+	}
+
+
+
+	public double getNoOfGoals() {
+		return noOfGoals;
+	}
+
+	public void setNoOfGoals(double noOfGoals) {
+		this.noOfGoals = noOfGoals;
+	}
+
+	public String getTeamName() {
+		return teamName;
+	}
+
+	public void setTeamName(String teamName) {
+		this.teamName = teamName;
+	}
+
+	@Override
+	public String toString() {
+		return "FootBallPlayer [noOfGoals=" + noOfGoals + ", teamName=" + teamName + "]" + super.toString();
+	}
+
+}
